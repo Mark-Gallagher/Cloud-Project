31d42ce4b45c0eb085b5e93c4890cd9202acbdc9
Mark-Gallagher <92432812+Mark-Gallagher@users.noreply.github.com>
2023-01-11 23:39:54 +0000

    Replaced Features.js with new code

--- components/Features.js
+++ components/Features.js
@@ -1,47 +1,86 @@
-import React from 'react';
-import styles from "../styles/Featured.module.css";
-import Image from "next/image";
-import { useState } from "react";
+import React from "react";
+import classes from "../styles/Featured.module.css";
+import { Container, Image } from "@nextui-org/react";
 
-function Features() 
-{
-    var i;
-    const [index,setIndex] = useState(0);
-    const image = [
-        "/images/header.jpg",
-        "/images/iced-cocao-expresso.jpg",
-        "/images/iced-white-expresso.jpg"
-        
-  ];
-
-  const handleArrow = (directions)=>{
-      if(directions ==="l"){
-          setIndex(index !== 0 ? index - 1 : 2)
-      }
-      if(directions ==="r"){
-        setIndex(index !== 2 ? index + 1 : 0)
-      }
-  }
-
-  
+function Features() {
   return (
-    <div className={styles.container}>
-      <div className={styles.arrowContainer} style={{left:0}} onClick={()=>handleArrow("l")}>
-        <Image src= "/images/arrowl.png" alt="" layout='fill' objectFit='contain'/>
+    <Container>
+      <div className={classes.mainDiv}>
+        <div className={classes.greenContainer}>
+          <div className={classes.greenContainerInner}>
+            <div className={classes.greenContainerP}>
+              <h3>Start fresh with Starbucks® Rewards</h3>
+              <a
+                className={classes.greenContainerL}
+                href="https://www.starbucks.com/account/create?_branch_match_id=1139309552818834311&utm_source=Web&utm_campaign=Homepage&utm_medium=Marketing&_branch_referrer=H4sIAAAAAAAAA8soKSkottLXLy5JLEoqTc4u1kssKNDLyczL1jcyDjAKzDX1SypJAgCic1QXJgAAAA%3D%3D"
+              >
+                Join
+              </a>{" "}
+              <p> today to earn your way to free coffee and treats.* </p>
+            </div>
+          </div>
         </div>
-
-        <div className={styles.wrapper} style={{transform: 'translateX(${-100*index}vw)'}}>
-          {image.map((img,i) =>(
-            <div className={styles.imgContainer} key ={i}>
-                <Image src={img} alt="" layout='fill' objectFit='contain'/>
-                </div>  
-              ))}
+        <div className={classes.drinkContainerOne}>
+          <div className={classes.drinkContainerInnerOne}>
+            <div className={classes.drinkContainerRightItemOne}>
+              <img
+                className={classes.drinkContainerImgOne}
+                src="./images/Pistachio-Cream-Coffee.webp"
+                alt="smooth coffee topped with pistachio cream cold foam."
+              ></img>
+            </div>
+            <div className={classes.drinkContainerLeftItemOne}>
+              <div className={classes.drinkContainerLeftInner}>
+                <h2 className={classes.drinkContainerHeader}>
+                  New year, Cool <br></br>new brew
+                </h2>
+                <br></br>
+                <div className={classes.drinkContainerLineOne}>
+                  Introducing Pistachio Cream Cold Brew — bold, smooth coffee
+                  topped with pistachio cream cold foam.
+                </div>
+                <a
+                  href="https://www.starbucks.com/menu/product/2123702/iced"
+                  className={classes.drinkContainerbutton}
+                >
+                  Try it now
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.arrowContainer}style={{right:0}} onClick={()=>handleArrow("r")}>
-          <Image src="/images/arrowr.png" layout='fill' alt="" objectFit='contain'/>
+        <div className={classes.drinkContainerTwo}>
+          <div className={classes.drinkContainerInnerTwo}>
+            <div className={classes.drinkContainerLeftItemTwo}>
+              <img
+                className={classes.drinkContainerImgtwo}
+                src="./images/Pistachio-Frappuccino.jpg"
+                alt=""
+              ></img>
+            </div>
+            <div className={classes.drinkContainerRightItemTwo}>
+              <div className={classes.drinkContainerRightInner}>
+                <h2 className={classes.drinkContainerHeaderTwo}>
+                  Pistachio your way
+                </h2>
+                <br></br>
+                <div className={classes.drinkContainerLineTwo}>
+                  Our creamy Pistachio Frappuccino® blended beverage satisfies
+                  all your winter cravings.
+                </div>
+                <a
+                  href="https://www.starbucks.com/menu/product/2123364/iced"
+                  className={classes.drinkContainerbuttonTwo}
+                >
+                  Order now
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+    </Container>
   );
 }
-  
-  export default Features;
+
+export default Features;
